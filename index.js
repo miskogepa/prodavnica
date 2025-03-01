2364331d60572b9bcf8de801a253a9d2a0462804
miskogepa <milovan.jankovic50m@gmail.com>
2025-03-01 23:27:28 +0100

    prvi

---
+++ index.js
@@ -0,0 +1,38 @@
+// Challenge: Import 'initializeApp' from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+// Challenge: Import 'getDatabase' from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+// ovo je kopirano kada sam napravio firebase project i napravio realtime database
+const appSettings = {
+    databaseURL: "https://prodavnica-c345b-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+//sada spajamo nas projekat sa firebase
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, 'shoppinglist');
+
+//uzimamo input iz inputa
+const inputFieldEl = document.getElementById('input-field');
+const buttonEl = document.getElementById('add-button');
+const shoppingListEl = document.getElementById("shopping-list")
+
+buttonEl.addEventListener("click", function () {
+    let inputValue = inputFieldEl.value;
+
+    push(shoppingListInDB, inputValue);
+    appendItemToShoppingListEl(inputValue)
+    clearInputFieldEl()
+
+});
+
+//funkcija za brisanje inputa
+function clearInputFieldEl() {
+    inputFieldEl.value = "";
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
